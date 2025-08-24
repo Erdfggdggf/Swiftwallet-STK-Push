f268324fd7c84a88ad00e96361e3ff469467da47
Erdfggdggf <148847255+Erdfggdggf@users.noreply.github.com>
2025-08-24 17:19:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://astonishing-cassata-3e8929.netlify.app'   // keep your frontend domain
+  origin: 'https://roaring-platypus-ae8776.netlify.app'   // keep your frontend domain
 }));
 
 // -------- Firebase Init --------
