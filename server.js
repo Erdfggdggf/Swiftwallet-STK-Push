8a178c024065079c3a55a92da641c2e18488a9f3
Erdfggdggf <148847255+Erdfggdggf@users.noreply.github.com>
2025-08-24 11:59:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://swiftloanke-com.onrender.com'
+  origin: 'https://swiftwallet2024.netlify.app'
 }));
 
 // -------- Firebase Init --------
